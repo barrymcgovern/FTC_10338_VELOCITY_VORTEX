3892a5caab3e06aa50f9fab985ee43092bbea7a3
Dark Matters FTC <denabots@gmail.com>
2016-11-07 20:49:32 -0500

    11/7/2016 2nd draft

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Master.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Master.java
@@ -16,10 +16,14 @@ public class Autonomous_Master extends Competition_Hardware {
         // Wait for the game to start (driver presses PLAY)
         idle();
         waitForStart();
-        encoderDrive(DRIVE_SPEED, "up", 10, 5); //Has the robot go forward at a set speed, in a set direction, for 10 inches with a 5 second timeout\\
-         while (rangeSensor.rawUltrasonic() > 10){
-             drive("left");
+        encoderDrive(DRIVE_SPEED, "up", 10, 5); //Has the robot go forward at a set speed, in a set direction, for 10 inches with a 5 second timeout
+        runtime.reset();
+         while (rangeSensor.rawUltrasonic() > 10 && (runtime.seconds() < 6)){
+             telemetry.addData("Range_Sensor", rangeSensor.rawUltrasonic());
+             drive("left"); //goes forward then after a certain distance it goes left until certain distance then stops
+
          }
+
     }
 
 public void initSystem(){
