82d0fabf257b7ca8e484f303b34cc00bb235659c
Barry <denaBots@gmail.com>
2017-02-01 13:07:46 -0500

    renamed some files and moved old files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Drive_Shoot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Drive_Shoot.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 /**
  * Created by kids on 1/20/2017.
  */
-@Autonomous(name="Comp: Drive Shoot", group="Pushbot")
+@Autonomous(name="Comp: Drive Shoot Only", group="Pushbot")
 
 public class Autonomous_Drive_Shoot extends Autonomous_Drive_Shoot_Beacon_New {
     @Override
