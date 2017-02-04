3f9353a979f5c60968db4548ac877f24258692e4
Dark Matters FTC <denabots@gmail.com>
2017-02-04 04:40:18 -0500

    2/1/17 Forklift Motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Drive_Only.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Drive_Only.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 /**
  * Created by kids on 1/20/2017.
  */
-@Autonomous(name="Comp: Drive Shoot Only", group="Pushbot")
+@Autonomous(name="Comp: Drive Only", group="Pushbot")
 
 public class Autonomous_Drive_Only extends Autonomous_Drive_Shoot_Beacon_New {
     @Override
