bd8e03d5d6dbde9a28181834951430d77be24bec
Dark Matters FTC <denabots@gmail.com>
2017-02-04 04:41:07 -0500

    2/1/17 Forklift Motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Drive_Shoot_Beacon_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Drive_Shoot_Beacon_Blue.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 //Runs Autonomous_Drive_and_Shoot with teamColor set to blue for correct directions
-@Autonomous(name="Comp: Blue Drive Shoot Beacon", group="Pushbot")
+@Autonomous(name="Comp: BLUE Drive Shoot Beacon", group="Pushbot")
 
 public class Autonomous_Drive_Shoot_Beacon_Blue extends Autonomous_Drive_Shoot_Beacon_New {
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Drive_Shoot_Beacon_Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Drive_Shoot_Beacon_Red.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 //Runs Autonomous_Drive_and_Shoot with teamColor set to red for correct directions
-@Autonomous (name= "Comp: Red Drive Shoot Beacon", group="Pushbot")
+@Autonomous (name= "Comp: RED Drive Shoot Beacon", group="Pushbot")
 public class Autonomous_Drive_Shoot_Beacon_Red extends Autonomous_Drive_Shoot_Beacon_New {
 
     @Override
